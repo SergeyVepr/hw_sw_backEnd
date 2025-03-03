e464841e7f8ba6d6f95014a85de9c0b161df3935
«SergeyVepr» <lolssan@icloud.com>
2025-03-03 20:27:47 +0200

    set appApl

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import {PlanetController} from "./controllers/PlanetController";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI!, {dbName: "starWars"})
+mongoose.connect(process.env.MONGO_URI!)
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) =>{
         console.error('MongoDb connection error: ' + err);
